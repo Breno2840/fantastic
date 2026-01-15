d12c7cc86252465641679b991c6af5e3967c00d0
Breno2840 <128945701+Breno2840@users.noreply.github.com>
2026-01-15 00:13:53 -0300

    Delete lib/core directory

--- lib/core/colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class PulsoColors {
-  static const background = Color(0xFF0B0F14);
-  static const surface = Color(0xFF101720);
-  static const divider = Color(0xFF1C2530);
-  static const textPrimary = Color(0xFFE6EDF3);
-  static const textSecondary = Color(0xFF9BA4AE);
-}

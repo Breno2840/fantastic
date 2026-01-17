852cb6f0a538f49509c89cc923a97bf4b18c883a
Breno2840 <128945701+Breno2840@users.noreply.github.com>
2026-01-17 18:31:02 -0300

    Criar core/app_colors.dart

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0xFF050505);
+  static const Color surface = Color(0xFF1A1A1A);
+  static const Color neonGreen = Color(0xFF00FF41);
+  static const Color neonCyan = Color(0xFF00E5FF);
+  static const Color textPrimary = Color(0xFFE0E0E0);
+}
